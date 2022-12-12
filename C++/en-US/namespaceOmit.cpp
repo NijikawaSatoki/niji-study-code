f5ec0914d05099b5cf6f6557ea38683b50ae441a
Satoki Nijikawa <120293916+NijikawaSatoki@users.noreply.github.com>
2022-12-12 18:46:46 -0500

    Update and rename C++/namespaceOmit.cpp to C++/en-US/namespaceOmit.cpp

---
+++ C++/en-US/namespaceOmit.cpp
@@ -0,0 +1,18 @@
+/* ===============================================
+ * Omitting namespace
+ * ===============================================
+ *
+ * Author: Satoki Nijikawa
+ * Version: 1.00a
+ *
+ */
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello world!";
+	std::cout << std::endl;
+	return 0;
+}
+
+// ... Nah, I's still rather just use 'using namespace std;'
