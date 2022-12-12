db6c69db7f85ee55b71ab2cca47dda9ddf05d811
Satoki Nijikawa <120293916+NijikawaSatoki@users.noreply.github.com>
2022-12-12 17:41:39 -0500

    Update and rename C++/Constant.cpp to C++/en-US/Constant.cpp

--- C++/en-US/Constant.cpp
+++ C++/en-US/Constant.cpp
@@ -12,7 +12,7 @@ using namespace std;
 
 int main() {
 	cout << "Hello world!" << endl;
-	const int quarterHour = 15; // Variable 'quarterHour' is 15
+	const int quarterHour = 15; // Variable 'quarterHour' is 15 and made read-only
 	cout << "The answer to 60/4 is " << quarterHour << ".";
 	cout << endl;
 	return 0;
