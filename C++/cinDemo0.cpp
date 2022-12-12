12a1da5632611abbd41906014cef2614387eb109
Satoki Nijikawa <120293916+NijikawaSatoki@users.noreply.github.com>
2022-12-12 18:26:23 -0500

    Update cinDemo0.cpp

--- C++/cinDemo0.cpp
+++ C++/cinDemo0.cpp
@@ -1,10 +1,9 @@
-/* ================================
- * Using 'cin' (Example 1)
- * ================================
+/* ===============================================
+ * C++ Input I
+ * ===============================================
  * 
  * Author: Satoki Nijikawa
- * Circle: Youkai Gekkou
- * Version: 0.01
+ * Version: 1.00a
  * 
  */
 
@@ -12,7 +11,6 @@
 using namespace std;
 
 int main() {
-	cout << "Hello world!" << endl;
 	int x; // Initialises x
 	cout << "Please enter a number: "; // Enter a number and hit RETURN
 	cin >> x; // Input value
