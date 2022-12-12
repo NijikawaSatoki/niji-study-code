79b2f9ccde67178625165de540c12e2f590b1512
Satoki Nijikawa <120293916+NijikawaSatoki@users.noreply.github.com>
2022-12-12 18:35:43 -0500

    Update and rename C++/floatDemo0.cpp to C++/en-US/floatDemo0.cpp

---
+++ C++/en-US/floatDemo0.cpp
@@ -0,0 +1,18 @@
+/* ===============================================
+ * Using float Variables I
+ * ===============================================
+ *
+ * Author: Satoki Nijikawa
+ * Version: 1.00a
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	float myNum = 5.75; // Variable 'myNum' is set to '5.75'
+	cout << "Five and three quarters is written as " << myNum << " .";
+	cout << endl;
+	return 0;
+}
