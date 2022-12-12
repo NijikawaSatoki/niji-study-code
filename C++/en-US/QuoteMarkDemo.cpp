400e483d0a5f1872cf41b025275532d63b516e89
Satoki Nijikawa <120293916+NijikawaSatoki@users.noreply.github.com>
2022-12-12 17:56:56 -0500

    Update and rename C++/QuoteMarkDemo.cpp to C++/en-US/QuoteMarkDemo.cpp

---
+++ C++/en-US/QuoteMarkDemo.cpp
@@ -0,0 +1,18 @@
+/* ===============================================
+ * Using Quote Marks In Strings
+ * ===============================================
+ *
+ * Author: Satoki Nijikawa
+ * Version: 1.00a
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello \"world\"! "; // This is will double quotation marks
+	cout << "I am learning C++!";
+	cout << endl;
+	return 0;
+}
