a27c1f91cb13ffa2ae7f777e15335d83966c2520
Satoki Nijikawa <120293916+NijikawaSatoki@users.noreply.github.com>
2022-12-12 18:12:36 -0500

    Update and rename C++/TabMojiDemo.cpp to C++/en-US/TabMojiDemo.cpp

---
+++ C++/en-US/TabMojiDemo.cpp
@@ -0,0 +1,18 @@
+/* ===============================================
+ * Using TAB Character In Strings
+ * ===============================================
+ *
+ * Author: Satoki Nijikawa
+ * Version: 1.00a
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello world!\t"; // This will insert a tab character
+	cout << "I am learning C++!";
+	cout << endl;
+	return 0;
+}
