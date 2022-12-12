4f535c9890a634ed282e8c05aad35d6ccc1a187a
Satoki Nijikawa <nijikawasatoki00@outlook.com>
2022-12-12 17:16:00 -0500

    Added more examples

---
+++ Python/Python-3/indent.py
@@ -0,0 +1,12 @@
+# ======================================
+# Indentation
+# ======================================
+# 
+# Author: Satoki Nijikawa
+# Circle: Youkai Gekkou
+# Version: 0.01a
+
+x = 1
+if x == 1:
+    # indented four spaces
+    print("x is 1.")
---
+++ Python/Python-3/printThisLine.py
@@ -0,0 +1,9 @@
+# ======================================
+# Print Line
+# ======================================
+# 
+# Author: Satoki Nijikawa
+# Circle: Youkai Gekkou
+# Version: 0.01a
+
+print("This line will be printed.")
