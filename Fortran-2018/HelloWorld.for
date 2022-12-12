efb7c1b244ca2f229a1fa2ccc2eafdb60b1b7e5a
Satoki Nijikawa <120293916+NijikawaSatoki@users.noreply.github.com>
2022-12-12 00:17:12 -0500

    Create HelloWorld.for

---
+++ Fortran-2018/HelloWorld.for
@@ -0,0 +1,11 @@
+! ==========================================================
+! Hello World
+! ==========================================================
+! 
+! Author: Satoki Nijikawa
+! Language: Fortran 2018
+! Locale: en-US
+
+program helloworld
+     print *, "Hello, World!"
+end program helloworld
