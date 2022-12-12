efdebaa5e082204ed583c8e8daebfa91ae8fb77f
Satoki Nijikawa <120293916+NijikawaSatoki@users.noreply.github.com>
2022-12-12 18:42:11 -0500

    Update and rename C++/intVar0.cpp to C++/en-US/intVar0.cpp

---
+++ C++/en-US/intVar0.cpp
@@ -0,0 +1,18 @@
+/* ===============================================
+ * Variable int I
+ * ===============================================
+ *
+ * Author: Satoki Nijikawa
+ * Version: 1.00a
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int myNum = 15;
+	cout << "My number is " << myNum << ".";
+	cout << endl;
+	return 0;
+}
