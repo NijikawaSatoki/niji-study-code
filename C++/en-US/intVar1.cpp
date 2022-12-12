e07b6904d72c8d838879b53121bd32a46ff46fe8
Satoki Nijikawa <120293916+NijikawaSatoki@users.noreply.github.com>
2022-12-12 18:43:18 -0500

    Update and rename C++/intVar1.cpp to C++/en-US/intVar1.cpp

--- C++/en-US/intVar1.cpp
+++ C++/en-US/intVar1.cpp
@@ -1,12 +1,11 @@
 /* =====================================================================
- * Learning Project 00 v0.01a
+ * Variable int II
  * =====================================================================
  *
  * Author: Satoki Nijikawa
+ * Version: 1.00a
  *
  */
-
-// Variable "int" (Example 2)
 
 #include <iostream>
 using namespace std;
@@ -14,7 +13,6 @@ using namespace std;
 int main() {
 	int myNum;
 	myNum = 15;
-	cout << "Hello world! ";
 	cout << "My number is " << myNum << ".";
 	cout << endl;
 	return 0;
