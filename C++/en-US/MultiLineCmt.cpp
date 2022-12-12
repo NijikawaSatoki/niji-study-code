8aa116d6ee5b911603aeccc15a9b70f8e06714f4
Satoki Nijikawa <120293916+NijikawaSatoki@users.noreply.github.com>
2022-12-12 17:55:24 -0500

    Update and rename C++/MultiLineCmt.cpp to C++/en-US/MultiLineCmt.cpp

---
+++ C++/en-US/MultiLineCmt.cpp
@@ -0,0 +1,22 @@
+/* ===============================================
+ * Multi-Line Comments
+ * ===============================================
+ * 
+ * Author: Satoki Nijikawa
+ * Version: 1.00a
+ * 
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	/* The code below will print
+	 *  Hello World!
+	 *  I'm learning C++!
+	 * to the terminal. */
+	cout << "Hello world! ";
+	cout << "I'm learning C++!";
+	cout << endl;
+	return 0;
+}
