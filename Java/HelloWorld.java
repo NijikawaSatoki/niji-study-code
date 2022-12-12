6fa3e3f6482313111e60006af35cc20ead26e9e4
Satoki Nijikawa <120293916+NijikawaSatoki@users.noreply.github.com>
2022-12-12 00:25:37 -0500

    Create HelloWorld.java

---
+++ Java/HelloWorld.java
@@ -0,0 +1,14 @@
+/* =========================================================
+ * Hello World
+ * =========================================================
+ *
+ * Author: Satoki Nijikawa
+ * Version: 1.00a
+ *
+ */
+
+public class Main {
+        public static void main(String[] args) {
+                System.out.println("Hello, World!");
+        }
+}
