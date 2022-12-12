24dc2239d6b0f9ce7b446fb679b01c0a88bb36cf
Satoki Nijikawa <120293916+NijikawaSatoki@users.noreply.github.com>
2022-12-12 17:49:27 -0500

    Update and rename C++/DeclareManyVar1.cpp to C++/en-US/DeclareManyVar1.cpp

---
+++ C++/en-US/DeclareManyVar1.cpp
@@ -0,0 +1,19 @@
+/* ===============================================
+ * Declaring Multiple Variables II
+ * ===============================================
+ *
+ * Author: Satoki Nijikawa
+ * Version: 1.00a
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x, y, z;
+	x = y = z = 50;
+	cout << "Your total comes out to " << x + y + z << ".";
+	cout << endl;
+	return 0;
+}
