13eef40e5951171331f1f2fcfd419558dd35213c
Satoki Nijikawa <120293916+NijikawaSatoki@users.noreply.github.com>
2022-12-12 00:38:57 -0500

    Create HelloWorld.pas

---
+++ Pascal/HelloWorld.pas
@@ -0,0 +1,13 @@
+(* =========================================================
+ * Hello World
+ * =========================================================
+ * 
+ * Author: Satoki Nijikawa
+ * Version: 1.00a
+ * 
+ *)
+
+program hello(output);
+begin
+    writeln('Hello, World!');
+end.
