251fd16245a6f82b742abf15a8ca0e762b18b694
Satoki Nijikawa <120293916+NijikawaSatoki@users.noreply.github.com>
2022-12-12 18:28:00 -0500

    Update and rename C++/coutDemo.cpp to C++/en-US/coutDemo.cpp

---
+++ C++/en-US/coutDemo.cpp
@@ -0,0 +1,17 @@
+/* ===============================================
+ * C++ Output
+ * ===============================================
+ *
+ * Author: Satoki Nijikawa
+ * Version: 1.00a
+ *
+ */
+ 
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	cout << "Hello world!" ; // Outputs "Hello world!"
+	cout << endl;
+	return 0;
+}
