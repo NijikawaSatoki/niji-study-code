13f95f48500619c342e25c6477509a46d1d92abd
Satoki Nijikawa <120293916+NijikawaSatoki@users.noreply.github.com>
2022-12-12 18:37:28 -0500

    Update intDemo0.cpp

--- C++/intDemo0.cpp
+++ C++/intDemo0.cpp
@@ -11,10 +11,8 @@
 using namespace std;
 
 int main() {
-	int myAge = 35;
+	int myAge = 35;	// I'm not 35, by the way
 	cout << "My age is " << myAge << " years old.";
 	cout << endl;
 	return 0;
 }
-
-// I'm not 35, by the way.
