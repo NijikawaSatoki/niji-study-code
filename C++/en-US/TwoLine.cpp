76f9e672c5e248e9c5e75f8bc3e54937fd5c5483
Satoki Nijikawa <120293916+NijikawaSatoki@users.noreply.github.com>
2022-12-12 18:17:27 -0500

    Update and rename C++/TwoLine.cpp to C++/en-US/TwoLine.cpp

---
+++ C++/en-US/TwoLine.cpp
@@ -0,0 +1,18 @@
+/* ===============================================
+ * Writing Code In Two Lines
+ * ===============================================
+ *
+ * Author: Satoki Nijikawa
+ * Version: 1.00a
+ *
+ */
+
+#include <iostream>
+using namespace std; int main() { cout << "Hello world! "; cout << endl; return 0; }
+
+/* If you do anything like the above, the stop 
+ * writing code. Why? Because the above makes 
+ * the code a pain to debug. Spaghetti code, of
+ * all things, is better than writing your code 
+ * in TWO DUCKING LINES.
+ * END OF RANT */
