d1ee21654f1793bd3a30fa4f72712580e5a0538c
Satoki Nijikawa <120293916+NijikawaSatoki@users.noreply.github.com>
2022-12-16 23:28:56 -0500

    Create BasicUserInput.sh

---
+++ bash/en-US/UserInput/BasicUserInput.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# ==========================================================
+# Basic User Input With bash
+# ==========================================================
+# 
+# Author: Satoki Nijikawa
+# Version: 1.00b
+
+## This script will read user input and print the result.
+echo -e "Please enter your name: "
+read name # Store the user input as variable $name
+echo "Nice to meet you, $name"
