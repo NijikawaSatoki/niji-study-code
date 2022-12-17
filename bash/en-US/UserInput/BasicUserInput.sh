3af8260b398466751bced2c47cb37cb76f753122
NijikawaSatoki <nijikawasatoki00@outlook.com>
2022-12-16 23:32:33 -0500

    Changed indent distance of comment on line 12

--- bash/en-US/UserInput/BasicUserInput.sh
+++ bash/en-US/UserInput/BasicUserInput.sh
@@ -9,5 +9,5 @@
 
 ## This script will read user input and print the result.
 echo -e "Please enter your name: "
-read name # Store the user input as variable $name
+read name	# Store the user input as variable $name
 echo "Nice to meet you, $name"
