bb89a47089396f58bf48153edec003d178037a93
Satoki Nijikawa <120293916+NijikawaSatoki@users.noreply.github.com>
2022-12-18 17:29:43 -0500

    Create Example.cpp

---
+++ C++/Reference/Libraries/string/Example.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string> // This is calling the string library.
+using namespace std;
+
+int main() {
+  string helloWorld = "Hello world!";
+  cout << helloWorld;
+  cout << endl;
+  return 0;
+}
